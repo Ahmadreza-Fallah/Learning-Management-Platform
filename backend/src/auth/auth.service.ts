895b1b0f6d56f3955db3bf7704c48ad0335c77f4
faeze dalili <faezedalili@gmail.com>
2026-07-02 12:38:24 +0330

    Generate JWT access token after successful authentication.

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -7,10 +7,13 @@ import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from '../prisma/prisma.service';
 import { LoginDto } from './dto/login.dto';
-
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
   async register(registerDto: RegisterDto) {
     const hashedPassword = await bcrypt.hash(registerDto.password, 10);
     const existingUser = await this.prisma.users.findFirst({
@@ -46,7 +49,7 @@ export class AuthService {
   }
 
   async login(loginDto: LoginDto) {
-    const user = this.prisma.users.findFirst({
+    const user = await this.prisma.users.findFirst({
       where: { UserName: loginDto.userName },
     });
     if (!user) {
@@ -55,7 +58,6 @@ export class AuthService {
     if (!user.IsActive) {
       throw new UnauthorizedException('User account is inactive');
     }
-
     const isPasswordValid = await bcrypt.compare(
       loginDto.password,
       user.PasswordHash,
@@ -64,9 +66,24 @@ export class AuthService {
       throw new UnauthorizedException('Invalid username or password');
     }
 
+    const payload = {
+      sub: user.Id,
+      username: user.UserName,
+      role: user.Role_Id,
+    };
+
+    const accessToken = await this.jwtService.signAsync(payload);
+
     return {
-      message: 'Login successful',
-      user,
+      accessToken,
+      user: {
+        id: user.Id,
+        firstName: user.FirstName,
+        lastName: user.LastName,
+        userName: user.UserName,
+        email: user.Email,
+        roleId: user.Role_Id,
+      },
     };
   }
 }
