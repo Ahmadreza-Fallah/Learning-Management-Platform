ff651d017cf277dbe436918f6860770d04b4072b
faeze dalili <faezedalili@gmail.com>
2026-07-27 00:13:15 +0330

    add frontend api calls

---
+++ frontend/src/services/upload.service.ts
@@ -0,0 +1,32 @@
+import api from "./api";
+
+class UploadService {
+  async uploadCourseImage(file: File) {
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    const response = await api.post("/upload/image", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data;
+  }
+  async uploadVideo(file: File) {
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    const response = await api.post("/upload/video", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data;
+  }
+}
+
+export default new UploadService();
