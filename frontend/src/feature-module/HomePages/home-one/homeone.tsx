1322d507e167f9807c74c36895868c282644580c
faeze dalili <faezedalili@gmail.com>
2026-07-31 00:40:31 +0330

    add category search and text search to home one

--- frontend/src/feature-module/HomePages/home-one/homeone.tsx
+++ frontend/src/feature-module/HomePages/home-one/homeone.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import BannerSection from "./section/banner";
 import Benefits from "./section/benefits";
-import Institutions from "./section/institutions";
 import Topcourses from "./section/top-courses";
-import Trust from "./section/trust";
 import Featuredcourse from "./section/featured-course";
-import Community from "./section/community";
 import Clinet from "./section/clinet";
 import Howitworks from "./section/how-it-works";
 import Featureinstructor from "./section/feature-instructor";
-import Faq from "./section/faq";
-import Testimonials from "./section/testimonials";
 import Footer from "./footer";
 
 const HomeOne = () => {
