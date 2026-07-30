038e579d058ad49367773dead4a05b2d28b7b580
faeze dalili <faezedalili@gmail.com>
2026-07-31 00:54:32 +0330

    add top categories to the main page

--- frontend/src/feature-module/HomePages/home-one/section/top-courses.tsx
+++ frontend/src/feature-module/HomePages/home-one/section/top-courses.tsx
@@ -1,162 +1,100 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import ImageWithBasePath from '../../../../core/common/imageWithBasePath';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import ImageWithBasePath from "../../../../core/common/imageWithBasePath";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { all_routes } from '../../../router/all_routes';
+import { all_routes } from "../../../router/all_routes";
+import toast from "react-hot-toast";
+import courseService, { Category } from "../../../../services/course.service";
+import { api_base_url } from "../../../../environment";
 
 const Topcourses = () => {
+  const [categoryId, setCategoryId] = useState("");
+  const [categories, setCategories] = useState<Category[]>([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        debugger;
+        const [cats] = await Promise.all([courseService.getCategories()]);
+        setCategories(cats);
+      } catch {
+        toast.error("خطا در بارگذاری اطلاعات");
+      }
+    };
 
-    //Top Course SLider
-    const topcourseslider = {
-        infinite: true,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 992,
-            settings: {
-              slidesToShow: 2,
-              infinite: true,
-              dots: false,
-            },
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 1,
-            },
-          },
-        ],
-      };
+    fetchData();
+  }, []);
+  //Top Course SLider
+  const topcourseslider = {
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
-      const route = all_routes
-      
-    return (
-        <>
-            {/* top courses */}
-            <section className="top-courses-sec">
-                <ImageWithBasePath className="top-courses-bg" src="./assets/img/bg/bg-20.png" alt="img" />
-                <div className="container">
-                    <div className="section-header text-center">
-                        <span className="fw-medium text-secondary text-decoration-underline mb-2 d-inline-block">
-                            Our Categories
-                        </span>
-                        <h2>Top Courses &amp; Categories</h2>
-                        <p>
-                            The right course, guided by an expert mentor, can provide invaluable
-                            insights, practical skills
-                        </p>
-                    </div>
-                        <Slider {...topcourseslider} className="top-courses-slider lazy">
-                        <div>
-                            <div className="categories-item categories-item-three mb-0">
-                                <ImageWithBasePath
-                                    className="mx-auto"
-                                    src="assets/img/category/icons/icon-6.svg"
-                                    alt="img"
-                                />
-                                <h6 className="title">
-                                    <Link to={route.courseCategory}>Frontend Developer</Link>
-                                </h6>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="categories-item categories-item-three mb-0">
-                                <ImageWithBasePath
-                                    className="mx-auto"
-                                    src="assets/img/category/icons/icon-7.svg"
-                                    alt="img"
-                                />
-                                <h6 className="title">
-                                    <Link to={route.courseCategory}>Jira Management</Link>
-                                </h6>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="categories-item categories-item-three mb-0">
-                                <ImageWithBasePath
-                                    className="mx-auto"
-                                    src="assets/img/category/icons/icon-8.svg"
-                                    alt="img"
-                                />
-                                <h6 className="title">
-                                    <Link to={route.courseCategory}>Figma Developer</Link>
-                                </h6>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="categories-item categories-item-three mb-0">
-                                <ImageWithBasePath
-                                    className="mx-auto"
-                                    src="assets/img/category/icons/icon-9.svg"
-                                    alt="img"
-                                />
-                                <h6 className="title">
-                                    <Link to={route.courseCategory}>Framer Developer</Link>
-                                </h6>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="categories-item categories-item-three mb-0">
-                                <ImageWithBasePath
-                                    className="mx-auto"
-                                    src="assets/img/category/icons/icon-10.svg"
-                                    alt="img"
-                                />
-                                <h6 className="title">
-                                    <Link to={route.courseCategory}>Vue js Developer</Link>
-                                </h6>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="categories-item categories-item-three mb-0">
-                                <ImageWithBasePath
-                                    className="mx-auto"
-                                    src="assets/img/category/icons/icon-11.svg"
-                                    alt="img"
-                                />
-                                <h6 className="title">
-                                    <Link to={route.courseCategory}>Shopify Developer</Link>
-                                </h6>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="categories-item categories-item-three mb-0">
-                                <ImageWithBasePath
-                                    className="mx-auto"
-                                    src="assets/img/category/icons/icon-10.svg"
-                                    alt="img"
-                                />
-                                <h6 className="title">
-                                    <Link to={route.courseCategory}>Vue js Developer</Link>
-                                </h6>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="categories-item categories-item-three mb-0">
-                                <ImageWithBasePath
-                                    className="mx-auto"
-                                    src="assets/img/category/icons/icon-11.svg"
-                                    alt="img"
-                                />
-                                <h6 className="title">
-                                    <Link to={route.courseCategory}>Shopify Developer</Link>
-                                </h6>
-                            </div>
-                        </div>
-                        </Slider>
-              
-                    <Link to={route.courseCategory} className="btn btn-primary btn-md">
-                        View All Categories
-                    </Link>
+  const handleCategory = (id: any) => {
+    const params = new URLSearchParams();
+    params.append("categoryId", id);
+    navigate(`${all_routes.courseGrid}?${params}`);
+  };
+  return (
+    <>
+      {/* top courses */}
+      <section className="top-courses-sec">
+        <ImageWithBasePath
+          className="top-courses-bg"
+          src="./assets/img/bg/bg-20.png"
+          alt="img"
+        />
+        <div className="container">
+          <div className="section-header text-center">
+            <span className="fw-medium text-secondary text-decoration-underline mb-2 d-inline-block">
+              دسته بندی دوره ها
+            </span>
+            <h2> دسته‌بندی‌های برتر دوره‌ها</h2>
+            <p>
+              یک دوره آموزشی مناسب که توسط مربی‌ای متخصص هدایت شود، می‌تواند
+              بینش‌های ارزشمند و مهارت‌های کاربردی فراهم آورد.
+            </p>
+          </div>
+          <Slider {...topcourseslider} className="top-courses-slider lazy m-3">
+            {categories?.map((cat) => (
+              <div key={cat?.Id} onClick={() => handleCategory(cat?.Id)}>
+                <div className="categories-item categories-item-three mb-0">
+                  <img
+                    className="mx-auto"
+                    src={`${api_base_url}${cat?.Icon}`}
+                    alt="img"
+                  />
+                  <h6 className="title">
+                    <Link to="#">{cat?.Title}</Link>
+                  </h6>
                 </div>
-            </section>
-            {/* /top courses */}
-        </>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </section>
+      {/* /top courses */}
+    </>
+  );
+};
 
-    )
-}
-
-export default Topcourses
+export default Topcourses;
